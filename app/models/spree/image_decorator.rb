15f6edacbf7fdf03ab68e66047f28d370fefc421
Lenny Sirivong <lenny@lenny-s.com>
2015-10-21 14:57:10 -0400

    Add support for  paperclips attachment(:style) method

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -6,4 +6,14 @@ Spree::Image.class_eval do
   def prepare_for_destroy; end
   def destroy_attached_files; end
   def validates_attachment; end
+
+  # handle shorthand attachment(:style_name)
+  def attachment_with_style(style = nil)
+    if style
+      attachment_without_style.url(style)
+    else
+      attachment_without_style
+    end
+  end
+  alias_method_chain :attachment, :style
 end
